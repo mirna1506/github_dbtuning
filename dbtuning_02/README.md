562c8734cd90b2a121f11ec0307a561605e45559
Ivica Brezovic <Ivica.Brezovic@stud.sbg.ac.at>
2020-03-17 23:02:12 +0100

    dbprogramm2 with all relevant informations

---
+++ dbtuning_02/README.md
@@ -0,0 +1,7 @@
+# DBTuning Programm 2
+## TODO
+localhost und server angaben umbennen!
+Bitte nur die beiden letzten Methoden (aktuell leer) anpassen. Vorlage ist das Projekt vom Martin.
+
+## Was kann das Programm grad?
+Alle Tabellen erzeugen, die Felder entsprechend der Angabe richtig befüllen. Man muss jetzt nurmehr die Queries schreiben.
--- dbtuning_02/src/dbtuning_02/QueryTuning.java
+++ dbtuning_02/src/dbtuning_02/QueryTuning.java
@@ -9,13 +9,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
 
 public class QueryTuning {
 	
-    private static ArrayList<Techdept> departments;
+	//Global Variables
+	private static ArrayList<Techdept> departments;
     private static Random gen = new Random();
     
     public static void main(String[] args) throws Exception {
@@ -34,15 +37,15 @@ public class QueryTuning {
 		/*String host = "biber.cosy.sbg.ac.at";
 		String port = "5432";
 		String database = "dbtuning_ss2020";
-		String pwd = "Iechei5eexai";
-		String user = "mmrazovic";*/
+		String pwd = "huth8lithe5E";
+		String user = "ibrezovic";*/
 
 		
 		//Localhost
 		String host = "localhost";
 		String port = "5432";
 		String database = "postgres";
-		String pwd = "lovenia";
+		String pwd = "postgres";
 		String user = "postgres";
 		
 		
@@ -60,104 +63,108 @@ public class QueryTuning {
 		//----------------------Sequence Order BEGIN----------------------
 		createandclean(con);
 		filltableswithdata(con);
+		orignalqueries(con);
+		rewrittenqueries(con);
+		System.out.println("DONE!");
 		//----------------------Sequence Order END------------------------
     }
     
-    //creates the empty tables or cleans them if they existed befores
+    //Creates the empty tables or cleans them if they existed befores
     private static void createandclean(Connection con) throws SQLException {
-        System.out.print("Cleaning db... \n");
+        System.out.print("Cleaning and Creating Tables... ");
         con.createStatement().execute("DROP TABLE IF EXISTS \"Employee\";");
         con.createStatement().execute("CREATE TABLE \"Employee\" (\n" +
-                                      "	ssnum INT NOT NULL UNIQUE,\n" +
-                                      "	name VARCHAR(40) NOT NULL UNIQUE,\n" +
-                                      "	manager VARCHAR(40) NOT NULL,\n" +
-                                      "	dept VARCHAR(40) NOT NULL,\n" +
-                                      "	salary NUMERIC(8,2) NOT NULL,\n" +
-                                      "	numfriends SMALLINT NOT NULL,\n" +
+                                      "	ssnum int UNIQUE,\n" +
+                                      "	name	text UNIQUE,\n" +
+                                      "	manager text,\n" +
+                                      "	dept text,\n" +
+                                      "	salary real,\n" +
+                                      "	numfriends int,\n" +
                                       " PRIMARY KEY (ssnum, name));");
         
         con.createStatement().execute("DROP TABLE IF EXISTS \"Student\";");
         con.createStatement().execute("CREATE TABLE \"Student\" (\n" +
-                                      "	ssnum INT NOT NULL UNIQUE,\n" +
-                                      "	name VARCHAR(40) NOT NULL UNIQUE,\n" +
-                                      "	course VARCHAR(40) NOT NULL,\n" +
-                                      "	grade SMALLINT NOT NULL,\n" +
+                                      "	ssnum int UNIQUE,\n" +
+                                      "	name text UNIQUE,\n" +
+                                      "	course text,\n" +
+                                      "	grade real,\n" +
                                       " PRIMARY KEY (ssnum, name));");
         
         con.createStatement().execute("DROP TABLE IF EXISTS \"Techdept\";");
         con.createStatement().execute("CREATE TABLE \"Techdept\" (\n" +
-                                      "	dept VARCHAR(40) NOT NULL UNIQUE,\n" +
-                                      "	manager VARCHAR(40) NOT NULL,\n" +
-                                      "	location VARCHAR(40) NOT NULL,\n" +
+                                      "	dept text UNIQUE,\n" +
+                                      "	manager text,\n" +
+                                      "	location text,\n" +
                                       " PRIMARY KEY (dept));");
         
+        con.createStatement().execute("CREATE UNIQUE INDEX ON \"Employee\" (ssnum);");
+        con.createStatement().execute("CREATE UNIQUE INDEX ON \"Employee\" (name);");
+        con.createStatement().execute("CREATE INDEX ON \"Employee\" (dept);");
         
-        con.createStatement().execute("CREATE UNIQUE INDEX employee_ssnum_unique_index ON \"Employee\" (ssnum);");
-        con.createStatement().execute("CREATE UNIQUE INDEX employee_name_unique_index ON \"Employee\" (name);");
-        con.createStatement().execute("CREATE INDEX employee_dept_index ON \"Employee\" (dept);");
+        con.createStatement().execute("CREATE UNIQUE INDEX ON \"Student\" (ssnum);");
+        con.createStatement().execute("CREATE UNIQUE INDEX ON \"Student\" (name);");
         
-        con.createStatement().execute("CREATE UNIQUE INDEX student_ssnum_unique_index  ON \"Student\" (ssnum);");
-        con.createStatement().execute("CREATE UNIQUE INDEX student_name_unique_index ON \"Student\" (name);");
-        
-        con.createStatement().execute("CREATE UNIQUE INDEX techdept_dept_unique_index ON \"Techdept\" (dept);");
+        con.createStatement().execute("CREATE UNIQUE INDEX ON \"Techdept\" (dept);");
         System.out.println("ok");
     }
     
-    //fills the tables with random data
-    private static void filltableswithdata(Connection con) throws SQLException, IOException {
-    	Random rand = new Random();
+    //Fills the tables with random data
+    private static void filltableswithdata(Connection con) throws SQLException, IOException {	
     	CopyManager cm = new CopyManager((BaseConnection) con);
     	FileOutputStream os = new FileOutputStream("Techdept.tsv");
         OutputStreamWriter wr = new OutputStreamWriter(os);
-        int ssnum = 0;
-        Techdept currdept;
-    	
-    	//Create 10 Techdepartments and save them in ArrayList
+        FileOutputStream os_student = new FileOutputStream("Student.tsv");
+        OutputStreamWriter wr_student = new OutputStreamWriter(os_student);
+        FileOutputStream os_employee = new FileOutputStream("Employee.tsv");
+        OutputStreamWriter wr_employee = new OutputStreamWriter(os_employee);
+
+        
+      //--------Create 10 Techdepartments and save them in ArrayList--------
+    	System.out.println("Creating Departments...");
     	createDepartments();
     	
-    	//Write departments to file - funktioniert 
+    	//--------Create Departments--------
     	for (Techdept dept : departments) {
             wr.write(dept.getDept() + "\t" + dept.getManager() + "\t" + dept.getLocation() + "\n");
         }
         wr.flush();
+        wr.close();
         cm.copyIn("COPY \"Techdept\" FROM stdin", new FileInputStream("Techdept.tsv"));
     	
-    	//Create 100k of students with an random techdepart out of array
+    	//--------Create 100k students--------
+        System.out.println("Creating Students...");
+    	for(int i=0; i<100000; i++) {
+    		//ssnum, name, course, grade //HIER VLT FÜR COURSE WAS SINNVOLLES NEHMEN
+    		wr_student.write(i + "\t" + UUID.randomUUID().toString() + "\t" + UUID.randomUUID().toString() + "\t" + ThreadLocalRandom.current().nextInt(1,6) + "\n");
+    	}
+    	wr_student.flush();
+    	wr_student.close();
+    	cm.copyIn("COPY \"Student\" FROM stdin", new FileInputStream("Student.tsv"));
     	
-    	//Create and write 100k of employees - funktioniert derzeit nicht
-      /*  os = new FileOutputStream("Employee.tsv");
-        wr = new OutputStreamWriter(os);
-        
-        for(int i = 1; i <= 99_990; i++) {
-        	int x = rand.nextInt((10 - 1) + 1) + 1;
-        	
-        	if(x == 1)
-        		wr.write(++ssnum + "\t" + randomString(10, 71) + "\t" + currdept.getManager() + "\t" + currdept.getDept());
-        	
-        }*/
-    	
+    	//--------Create 100k employees--------
+    	System.out.println("Creating Employees...");
+    	for(int j=100000; j<200000; j++) {
+    		String manager = null;
+    		String dept = null;
+    		//In ~10% der Fälle soll random ein manager und ein dept gesetzt werden
+    		if(ThreadLocalRandom.current().nextInt(1,11) == 5) {
+    			int random = ThreadLocalRandom.current().nextInt(0,10);
+    			manager = departments.get(random).getManager();
+    			dept = departments.get(random).getDept();
+    		}
+    		
+    		//ssnum, name, manager, dept, salary, numfriends
+    		wr_employee.write(j + "\t" + UUID.randomUUID().toString() + "\t" + manager + "\t" + dept + "\t" + ThreadLocalRandom.current().nextInt(2000,8000) + "\t" + ThreadLocalRandom.current().nextInt(2,12) +"\n");
+    	}
+    	wr_employee.flush();
+    	wr_employee.close();
+    	cm.copyIn("COPY \"Employee\" FROM stdin", new FileInputStream("Employee.tsv"));
     }
-    
-    //kopiert von Martin, bin nicht sicher ob es alles funktioniert
-    private static String randomString(int minLength, int bound) {
-    	
-    	int strLength = minLength + gen.nextInt(bound);
-        StringBuilder str = new StringBuilder();
-        
-        for (int i = 1; i <= strLength; i++) {
-            str.append('a' + gen.nextInt(26));
-        }
-        
-        return str.toString();
-    }
-    
     
     //Create 10 Techdepartments and save them in ArrayList
     private static void createDepartments() {
-    	
     	String[] managerPool = {"Mia Wagner", "David Gruber", "Maria Winkler", "Sophia Huber", "Helena Weber", "Emma Kocher", "Max Steiner", "Philipp Moser", "Fabian Mayer", "Franz Pichler"};
-        String[] locationPool = {"Salzburg", "Wien", "Graz", "Klagenfurt", "Villach", "Innsbruck", "Linz", "Bregenz", "Eisenstadt", "Wels"};
-         
+        String[] locationPool = {"Salzburg", "Wien", "Graz", "Klagenfurt", "Villach", "Innsbruck", "Linz", "Bregenz", "Eisenstadt", "Wels"};     
         departments = new ArrayList<Techdept>(10);
          
         departments.add(new Techdept("Development", managerPool[gen.nextInt(10)], locationPool[gen.nextInt(10)]));
@@ -171,9 +178,19 @@ public class QueryTuning {
         departments.add(new Techdept("IT Security", managerPool[gen.nextInt(10)], locationPool[gen.nextInt(10)]));
         departments.add(new Techdept("Software Testing", managerPool[gen.nextInt(10)], locationPool[gen.nextInt(10)]));
     }
+
+    //Hier zwei Queries schreiben, Zeit messen -> Siehe Programm1 !
+    public static void orignalqueries(Connection con) throws SQLException{
+    	
+    }
     
+    //Hier die zwei Queries umschreiben, Zeit messen -> Siehe Programm1 !
+    public static void rewrittenqueries(Connection con) throws SQLException{
+    	
+    }
+
 private static class Techdept {
-	    
+    
     private String dept;
     private String manager;
     private String location;
@@ -197,5 +214,5 @@ private static class Techdept {
     	}
     
     }
-
 }
+
